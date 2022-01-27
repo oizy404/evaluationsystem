dd6602a531453b7a887c571625a18d3cfebfe5ea
oizy404 <71779024+oizy404@users.noreply.github.com>
2022-01-27 10:08:26 +0800

    Update teacher-create.blade.php

--- resources/views/views-evaluation/teacher-create.blade.php
+++ resources/views/views-evaluation/teacher-create.blade.php
@@ -79,8 +79,8 @@
 
 <!--modal -->
   <div class="modal fade" id="tableModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog col-md-12" role="document">
-      <div class="modal-content">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content" style="width: 135%; margin-left: -50px;">
         <div class="modal-header">
           <h5 class="modal-title" id="exampleModalLabel">Search Teacher</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
