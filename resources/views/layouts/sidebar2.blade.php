eb29adca5b89275e1dbb64072986b537e24464ac
oizy404 <71779024+oizy404@users.noreply.github.com>
2022-02-03 09:35:38 +0800

    evaluationsystem

--- resources/views/layouts/sidebar2.blade.php
+++ resources/views/layouts/sidebar2.blade.php
@@ -3,8 +3,8 @@
           <ul class="nav flex-column">
                 <li class="nav-item"><a class="nav-link " href="{{route('admins.index')}}" class="text-dark">Administrator</a></li>
                 <li class="nav-item"><a class="nav-link " href="{{route('supervisors.index')}}" class="text-dark">Supervisor</a></li>
-                <li class="nav-item"><a class="nav-link " href="{{route('BEDevaluees.index')}}" class="text-dark">BED Teacher</a></li>
-                <li class="nav-item"><a class="nav-link " href="{{route('evaluees.index')}}" class="text-dark">College Teacher</a></li>
+                <!-- <li class="nav-item"><a class="nav-link " href="{{route('BEDevaluees.index')}}" class="text-dark">BED Teacher</a></li>
+                <li class="nav-item"><a class="nav-link " href="{{route('evaluees.index')}}" class="text-dark">College Teacher</a></li> -->
                 <li class="nav-item"><a class="nav-link " href="{{route('NTPevaluees.index')}}" class="text-dark">NTP</a></li>
           </ul>
         </div>
--- resources/views/views-employee/dashboard.blade.php
+++ resources/views/views-employee/dashboard.blade.php
@@ -15,4 +15,44 @@
         </center>
     </div>
 
+    <div class="container">
+        <div class="col-md-8 offset-md-2 administrator-container">
+            <div class="card">
+                <div class="card-body">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                            </tr>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <th></th>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 offset-md-2 supervisor-container">
+            <div class="card">
+                <div class="card-body">
+                    
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8 offset-md-2 ntp-container">
+            <div class="card">
+                <div class="card-body">
+
+                </div>
+            </div>
+        </div>
+    </div>
+
 @stop
