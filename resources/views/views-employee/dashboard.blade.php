1937faa19a541fa14d4ea8faa066696f7a4de229
Rllein1 <73153282+Rllein1@users.noreply.github.com>
2022-02-21 09:27:57 +0800

    Revert "sadasd"
    This reverts commit 1b9b867155beab5f2807f1f0c3a4a9c174f3fea1.

--- resources/views/views-employee/dashboard.blade.php
+++ resources/views/views-employee/dashboard.blade.php
@@ -6,7 +6,7 @@
 
 
    
-   <div class="mt-5">
+   <div class="mt-5 dashboard">
         <center>
         <h1>Welcome!</h1>
         <img src="{{asset('image/check2.gif')}}" style="width: 30%; height: 30%">
@@ -16,9 +16,10 @@
     </div>
 
     <div class="container">
-        <div class="col-md-8 offset-md-2 administrator-container">
+        <div class="col-md-8 offset-md-2 mt-5 administrator-container" style="display:none;">
             <div class="card">
                 <div class="card-body">
+<<<<<<< HEAD
                 <table class="table table-hover table-bordered evalueeTable" style="width:100%" id="evalueeTable">
                     <thead>
                         <tr>
@@ -29,6 +30,17 @@
                         </tr>
                     </thead>
  
+=======
+                    <table class="table table-hover table-bordered employeeTable" style="width:100%" id="employeeTable">
+                        <thead>
+                            <tr>
+                                <th style="display:none;">Name</th>
+                                <th class="bg-info text-dark">Name</th>
+                                <th class="bg-info text-dark">Last Name</th>
+                                <th class="bg-info text-dark">Office</th>
+                            </tr>
+                        </thead>
+>>>>>>> 3b413943b65f6201b3a6528b5e000f932d68c134
                         <tbody>
                             @foreach($evaluations as $evaluation)
                             @if($evaluation->tool_id==3)
@@ -41,18 +53,14 @@
                             @endif
                             @endforeach
                         </tbody>
-                        <tfoot>
-                            <tr>
-                                <th></th>
-                            </tr>
-                        </tfoot>
                     </table>
                 </div>
             </div>
         </div>
-        <div class="col-md-8 offset-md-2 supervisor-container">
+        <div class="col-md-8 offset-md-2 mt-5 supervisor-container" style="display:none;">
             <div class="card">
                 <div class="card-body">
+<<<<<<< HEAD
                 <table class="table table-hover table-bordered evalueeTable" style="width:100%" id="evalueeTable">
                     <thead>
                         <tr>
@@ -63,6 +71,17 @@
                         </tr>
                     </thead>
  
+=======
+                <table class="table table-hover table-bordered employeeTable" style="width:100%" id="employeeTable">
+                        <thead>
+                            <tr>
+                                <th style="display:none;">Name</th>
+                                <th class="bg-info text-dark">Name</th>
+                                <th class="bg-info text-dark">Last Name</th>
+                                <th class="bg-info text-dark">Office</th>
+                            </tr>
+                        </thead>
+>>>>>>> 3b413943b65f6201b3a6528b5e000f932d68c134
                         <tbody>
                             @foreach($evaluations as $evaluation)
                             @if($evaluation->tool_id==4)
@@ -84,9 +103,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8 offset-md-2 ntp-container">
+        <div class="col-md-8 offset-md-2 mt-5 ntp-container" style="display:none;">
             <div class="card">
                 <div class="card-body">
+<<<<<<< HEAD
                 <table class="table table-hover table-bordered evalueeTable" style="width:100%" id="evalueeTable">
                     <thead>
                         <tr>
@@ -97,6 +117,17 @@
                         </tr>
                     </thead>
  
+=======
+                <table class="table table-hover table-bordered employeeTable" style="width:100%" id="employeeTable">
+                        <thead>
+                            <tr>
+                                <th style="display:none;">Name</th>
+                                <th class="bg-info text-dark">Name</th>
+                                <th class="bg-info text-dark">Last Name</th>
+                                <th class="bg-info text-dark">Office</th>
+                            </tr>
+                        </thead>
+>>>>>>> 3b413943b65f6201b3a6528b5e000f932d68c134
                         <tbody>
                             @foreach($evaluations as $evaluation)
                             @if($evaluation->tool_id==5)
@@ -109,24 +140,50 @@
                             @endif
                             @endforeach
                         </tbody>
-                        <tfoot>
-                            <tr>
-                                <th></th>
-                            </tr>
-                        </tfoot>
                     </table>
                 </div>
             </div>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script>
+        $('.data').click(function(){
+            var code=$(this).find(":first-child").text();
+            window.location.replace(code);
+        });
+    </script>
+@stop
+@section('scripts')    
+<script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
+<script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
 <script>
-    $('.data').click(function(){
-        var code=$(this).find(":first-child").text();
-        window.location.replace(code);
-    })
+    
+    // $('#employeeTable').dataTable(); 
+    $(document).ready(function(){
+        $('.employeeTable').dataTable(); 
+    });
+        
+        $('#administrators').click(function(){
+            $('.administrator-container').fadeIn(300);
+            $('.supervisor-container').fadeOut(300);
+            $('.ntp-container').fadeOut(300);
+            $('.dashboard').fadeOut(300);
+        });
+        $('#supervisors').click(function(){
+            $('.supervisor-container').fadeIn(300);
+            $('.administrator-container').fadeOut(300);
+            $('.ntp-container').fadeOut(300);
+            $('.dashboard').fadeOut(300);
+        });
+        $('#ntp').click(function(){
+            $('.ntp-container').fadeIn(300);
+            $('.supervisor-container').fadeOut(300);
+            $('.administrator-container').fadeOut(300);
+            $('.dashboard').fadeOut(300);
+        })
 </script>
 @stop
+<<<<<<< HEAD
 @section('scripts')
     <script src="{{asset('js/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('js/dataTables.bootstrap4.min.js')}}"></script>
@@ -137,3 +194,6 @@ $(document).ready(function() {
 });    
 </script>
 @stop
+=======
+
+>>>>>>> 3b413943b65f6201b3a6528b5e000f932d68c134
